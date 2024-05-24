ceedd3edeac24315873b5935faf98c50c85754fc
life <>
2024-05-24 14:14:48 +0800

    图片处理

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -7,11 +7,21 @@ import '../generated/assets.dart';
 /// 图片加载工具类
 class ImageUtils {
   /// 加载本地资源图片
-  static Widget loadAssetImage(String assetsUrl, {double? width, double? height, BoxFit? fit, Color? color}) {
+  static Widget loadAssetImage(String assetsUrl, {double? width, double? height, double radius = 0, BoxFit? fit}) {
+    if (radius > 0) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(radius),
+        child: Image.asset(
+          assetsUrl,
+          height: height,
+          width: width,
+          fit: fit,
+        ),
+      );
+    }
     return Image.asset(
       assetsUrl,
       height: height,
-      color: color,
       width: width,
       fit: fit,
     );
@@ -48,29 +58,38 @@ class ImageUtils {
   }
 
   ///加载加密的图片
-  static Widget loadEncryptImage(String url,{BoxFit? fit,double radius = 0,}){
-    if(radius > 0){
+  static Widget loadEncryptImage(
+    String url, {
+    BoxFit? fit,
+    double radius = 0,
+  }) {
+    if (radius > 0) {
       return ClipRRect(
         borderRadius: BorderRadius.circular(radius),
-        child: CustomDecryptImage(url,fit: fit),
+        child: CustomDecryptImage(url, fit: fit),
       );
     }
-    return CustomDecryptImage(url,fit: fit,);
+    return CustomDecryptImage(
+      url,
+      fit: fit,
+    );
   }
 }
 
 extension ImageLoadExt on String {
-
   ///字符串快速转网络图片加载widget
-  Widget toNetworkImageWidget({double? width, double? height, BoxFit? fit}) {
-    return ImageUtils.loadNetworkImage(this,width: width,height: height);
+  Widget toNetworkImageWidget({double? width, double? height, BoxFit? fit, radius}) {
+    return ImageUtils.loadNetworkImage(this, width: width, height: height, radius: radius);
   }
 
   ///图片地址快速加载加密的网络图片
-  Widget toEncryptNetworkImageWidget({BoxFit? fit,double radius = 0,}){
-    if(radius > 0){
+  Widget toEncryptNetworkImageWidget({
+    BoxFit? fit,
+    double radius = 0,
+  }) {
+    if (radius > 0) {
       return ClipRRect(
-          borderRadius: BorderRadius.circular(radius),
+        borderRadius: BorderRadius.circular(radius),
         child: CustomDecryptImage(this),
       );
     }
@@ -78,7 +97,18 @@ extension ImageLoadExt on String {
   }
 
   ///字符串转 assets 图片加载
-  Widget toAssetImageWidget({double? width, double? height, BoxFit? fit, Color? color}){
-    return ImageUtils.loadAssetImage(this,width: width,height: height,fit: fit,color: color);
+  Widget toAssetImageWidget({double? width, double? height, BoxFit? fit, double radius = 0}) {
+    return ImageUtils.loadAssetImage(this, width: width, height: height, fit: fit, radius: radius);
+  }
+
+  ///字符串加载图片
+  Widget toImageWidget({
+    BoxFit? fit,
+    double radius = 0,
+  }) {
+    if (startsWith("http")) {
+      return toNetworkImageWidget(fit: fit, radius: radius);
+    }
+    return toAssetImageWidget(fit: fit, radius: radius);
   }
 }
