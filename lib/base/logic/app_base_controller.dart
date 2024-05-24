d099974193c5a232cbdb065d7a69413219b580f0
life <>
2024-05-24 12:48:29 +0800

    加载框

--- lib/base/logic/app_base_controller.dart
+++ lib/base/logic/app_base_controller.dart
@@ -1,15 +1,13 @@
-import 'package:dio/dio.dart';
-import 'package:flutter_app/base/app_dialog_mixin.dart';
+
 import 'package:get/get.dart';
 
 import '../widget/loading_layout.dart';
+import 'app_base_network_controller.dart';
 
 /// 1. 页面的状态管理
-abstract class AppBaseController extends GetxController with AppDialogMixin{
+abstract class AppBaseController extends GetxController with AppBaseNetworkController{
   /// 页面加载状态 在自定义加载类中
   var loadingState = LoadingState.loading.obs;
-  /// 网络请求取消用
-  CancelToken cancelToken = CancelToken();
 
   /// 初始化
   @override
@@ -26,7 +24,7 @@ abstract class AppBaseController extends GetxController with AppDialogMixin{
   /// 销毁
   @override
   void onClose() {
-    cancelToken.cancel();
+    onNetworkClose();
     super.onClose();
   }
 
--- lib/utils/app_dialog_utils.dart
+++ lib/utils/app_dialog_utils.dart
@@ -2,20 +2,25 @@ import 'package:flutter_app/widget/app_dialog_widget.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 
 ///弹窗扩展实现
-mixin AppDialogMixin{
+class AppDialogUtils{
 
   ///显示加载框
-  void showLoading() {
+  static void showLoadingDialog() {
     SmartDialog.showLoading(msg: "正在加载...");
   }
 
+  ///关闭加载框
+  static void dismissLoadingDialog(){
+    SmartDialog.dismiss();
+  }
+
   ///显示提示文本
-  void showToast(String msg){
+  static void showToast(String msg){
     SmartDialog.showToast(msg);
   }
 
   ///显示确认对话框
-  void showConfirmDialog(String title,String message,Function onConfirm){
+  static void showConfirmDialog(String title,String message,Function onConfirm){
     SmartDialog.show(builder: (context){
       return AppDialogWidget.buildDefault(title, message,onConfirm: onConfirm);
     });
